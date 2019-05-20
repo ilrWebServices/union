ea7aa4bd0bdcb85907f9e8d796054d8934354762
Jeff Amaral <jeff.amaral@cornell.edu>
2019-05-20 09:01:16 -0700

    Add gulp livereload
    This is temporary until pattern-lab css reloading is working properly.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const concat = require('gulp-concat');
+const livereload = require("gulp-livereload");
 
 var sass_config = {
   outputStyle: "nested"
@@ -30,14 +31,26 @@ function concatScripts() {
     .pipe(gulp.dest('source/js/', { sourcemaps: '.' }));
 }
 
-function watchFiles() {
+function livereloadStartServer(done) {
+  livereload.listen({ 'port': 35778 });
+  done();
+}
+
+function watchFiles(done) {
   gulp.watch('source/patterns/**/*.scss', allTasks);
+
+  var lr_watcher = gulp.watch([
+    'public/css/union.css'
+  ]);
+  lr_watcher.on('change', livereload.changed);
+  done();
 }
 
 const allStyles = gulp.parallel(componentStyles, concatComponentStyles);
 const allTasks = gulp.parallel(allStyles, concatScripts);
+const watch = gulp.parallel(watchFiles, livereloadStartServer);
 
 // Export gulp tasks.
 exports.styles = allStyles;
 exports.all = allTasks;
-exports.default = watchFiles;
+exports.default = watch;
