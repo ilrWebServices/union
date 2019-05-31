2000cda083ef920f50791966ab64669d7e798b46
Jeff Amaral <jeff.amaral@cornell.edu>
2019-05-31 11:54:15 -0700

    Update form js to work even when form elements are replaced in the DOM (e.g. ajax)
    Uses event delegation and MutationObserver.

--- source/patterns/components/form/form.js
+++ source/patterns/components/form/form.js
@@ -1,55 +1,49 @@
 (function (document) {
-  const inputs = document.querySelectorAll('.cu-input');
 
-  for (let i = 0, len = inputs.length; i < len; i++) {
-    const input = inputs[i];
-    const wrapper = input.closest('.cu-input-list__item');
+  document.addEventListener('change', function(event) {
+    if (event.target.matches('.cu-input')) {
+      let wrapper = event.target.closest('.cu-input-list__item');
 
-    // Check if the field has pre-filled text from the server side
-    if (input.value) {
-      input.classList.add('is-filled');
-      // Check if input has a wrapper
-      if (wrapper) {
+      if (event.target.value) {
+        event.target.classList.add('is-filled');
         wrapper.classList.add('is-filled');
       }
-    }
-
-    // If wrapper, remove the js-disabled class for float labels
-    if (wrapper) {
-      wrapper.classList.remove('js-disabled');
-    }
-
-    input.onchange = function() {
-      let wrapper = input.closest('.cu-input-list__item');
-      if (input.value) {
-        input.classList.add('is-filled');
-        wrapper.classList.add('is-filled');
-      } else {
-        input.classList.remove('is-filled');
+      else {
+        event.target.classList.remove('is-filled');
         wrapper.classList.remove('is-filled');
       }
-    };
+    }
+  }, false);
 
-    input.onfocus = function() {
-      input.classList.remove('is-touched');
-      input.classList.add('is-focused');
+  // This is the equivalent of focus (which doesnt' bubble).
+  document.addEventListener('focusin', function(event) {
+    if (event.target.matches('.cu-input')) {
+      let wrapper = event.target.closest('.cu-input-list__item');
 
-      // Check for wrapper and update classes
+      event.target.classList.remove('is-touched');
+      event.target.classList.add('is-focused');
+
+      // Check for wrapper and update classes.
       if (wrapper) {
         wrapper.classList.add('is-active')
       }
 
-      // In there were server-side errors, the 'is-invalid' class will be present
+      // If there were server-side errors, the 'is-invalid' class will be present
       // but should be removed on focus because the user is trying to fix them.
-      input.classList.remove('is-invalid');
+      event.target.classList.remove('is-invalid');
 
-      if (input.errors) {
-        input.errors.remove();
+      if (event.target.errors) {
+        event.target.errors.remove();
       }
-    };
+    }
+  }, false);
 
-    input.onblur = function(e) {
-      if (!e.isTrusted) {
+  // This is the equivalent of blur (which doesnt' bubble).
+  document.addEventListener('focusout', function(event) {
+    if (event.target.matches('.cu-input')) {
+      let wrapper = event.target.closest('.cu-input-list__item');
+
+      if (!event.isTrusted) {
         // This blur event was triggered by a script, not a human, so don't mark
         // the input as is-touched (because it actually wasn't) or show errors.
 
@@ -59,12 +53,61 @@
         return;
       }
 
-      input.classList.add('is-touched');
+      event.target.classList.add('is-touched');
+      event.target.classList.remove('is-focused');
 
       // Check for wrapper and update classes
       if (wrapper) {
         wrapper.classList.remove('is-active');
       }
-    };
-  }
+    }
+  }, false);
+
+  // Process any .cu-input elements whenever stuff is added to the DOM. This
+  // includes the initial page load.
+  let observer = new MutationObserver(function(mutations, observer) {
+    // Check to see if any of the mutations added nodes to the DOM.
+    let added_children = false;
+
+    for (const mutation of mutations) {
+      if (mutation.type === 'childList' && mutation.addedNodes.length) {
+        added_children = true;
+        break;
+      }
+    }
+
+    // Return if not adding children.
+    if (!added_children) {
+      return;
+    }
+
+    const inputs = document.querySelectorAll('.cu-input');
+
+    for (const input of inputs) {
+      const wrapper = input.closest('.cu-input-list__item');
+
+      // Check if the field has pre-filled text from the server side.
+      if (input.value) {
+        input.classList.add('is-filled');
+
+        // Check if input has a wrapper.
+        if (wrapper) {
+          wrapper.classList.add('is-filled');
+        }
+      }
+
+      // If wrapper, remove the js-disabled class for float labels.
+      if (wrapper) {
+        wrapper.classList.remove('js-disabled');
+      }
+    }
+  });
+
+  // @todo Investigate the performance ramifications of observing the entire
+  // document, plus its children.
+  observer.observe(document, {
+    childList: true,
+    subtree: true
+  });
+
 })(document);
