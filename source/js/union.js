98af1f820b7458e5bab97be272c65fba196a4e44
Aaron Froehlich <aaronfroehlich@gmail.com>
2019-08-06 11:34:58 -0400

    Add initial banner with video

--- source/js/union.js
+++ source/js/union.js
@@ -1,3 +1,27 @@
+(function (document) {
+  const video_trigger_present = document.querySelectorAll('.cu-icon--play').length;
+  if (video_trigger_present) {
+    document.addEventListener("click", function (event) {
+      if (event.target.matches('.cu-icon--play')) {
+        let videoId = event.target.id.replace('toggle', '');
+        let videoElement = document.getElementById(videoId);
+        // @todo - refactor to something more flexible/predictable
+        let overlayContent = event.target.closest('div').parentElement;
+        handlePlayButton(videoElement, overlayContent);
+        event.target.style.opacity = 1;
+      }
+
+      function handlePlayButton(videoElement, overlayContent) {
+        if (videoElement.paused) {
+          videoElement.play();
+          overlayContent.style.display = 'none';
+          videoElement.controls = true;
+        }
+      }
+    });
+  }
+})(document);
+
 (function (window, document) {
 
   if (('localStorage' in window) === false) {
