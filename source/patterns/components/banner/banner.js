02de4d9f592bcccf205ed2912350a977386bac77
Jeff Amaral <jeff.amaral@cornell.edu>
2019-08-15 11:37:54 -0400

    Refactor video banner play icon javascript

--- source/patterns/components/banner/banner.js
+++ source/patterns/components/banner/banner.js
@@ -1,23 +1,19 @@
 (function (document) {
-  const video_trigger_present = document.querySelectorAll('.cu-icon--play').length;
-  if (video_trigger_present) {
-    document.addEventListener("click", function (event) {
-      if (event.target.matches('.cu-icon--play')) {
-        let videoId = event.target.id.replace('toggle', '');
-        let videoElement = document.getElementById(videoId);
-        // @todo - refactor to something more flexible/predictable
-        let overlayContent = event.target.closest('div').parentElement;
-        handlePlayButton(videoElement, overlayContent);
-        event.target.style.opacity = 1;
-      }
 
-      function handlePlayButton(videoElement, overlayContent) {
-        if (videoElement.paused) {
-          videoElement.play();
-          overlayContent.style.display = 'none';
-          videoElement.controls = true;
-        }
+  // Wire play icons to videos in banners.
+  document.addEventListener('click', function(event) {
+    if (event.target.matches('.cu-icon--play')) {
+      // If the icon has a video target selector in a `data-cu-video-target`
+      // attribute, use that. Otherwise, use the closest video.
+      let video_element = document.querySelector(event.target.dataset.cuVideoTarget) || event.target.closest('.cu-banner').querySelector('.cu-banner__media video');
+      let overlay_content = event.target.closest('.cu-banner__content');
+
+      if (video_element && video_element.paused) {
+        video_element.play();
+        overlay_content.style.display = 'none';
+        video_element.controls = true;
       }
-    });
-  }
+    }
+  });
+
 })(document);
