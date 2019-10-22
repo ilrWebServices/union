e98e4f621c245b71a953f7ea3c20464ad4a1baa3
Aaron Froehlich <aaronfroehlich@gmail.com>
2019-10-22 12:00:57 -0400

    Add initial responsive behavior to the course finder

--- source/js/union.js
+++ source/js/union.js
@@ -18,6 +18,38 @@
 
 })(document);
 
+(function (document) {
+
+  const displayTopicList = function () {
+    const listContainers = document.querySelectorAll('.cu-course-finder__topics-list');
+
+    // Show the list.
+    listContainers.forEach(function (container) {
+      container.classList.add('topics-list--expanded');
+    });
+  }
+
+  document.addEventListener('click', function (event) {
+    if (event.target.matches('.cu-course-finder__trigger')) {
+      displayTopicList(event.target);
+    }
+  }, false);
+
+  // Able to close topic list by hitting escape
+  document.addEventListener('keydown', function (event) {
+    if (event.key === "Escape") {
+      // Find element that was active when escape was hit
+      const activeContainers = document.querySelectorAll('.topics-list--expanded');
+
+      // Hide the list.
+      activeContainers.forEach(function (container) {
+        container.classList.remove('topics-list--expanded');
+      });
+    }
+  });
+
+})(document);
+
 (function (window, document) {
 
   if (('localStorage' in window) === false) {
