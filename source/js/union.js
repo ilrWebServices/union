416b4f50d9dc3bd38e82b7eb304937d5b74bc3cc
Aaron Froehlich <aaronfroehlich@gmail.com>
2020-08-05 14:49:11 -0400

    Fix play/pause button on video banners

--- source/js/union.js
+++ source/js/union.js
@@ -6,12 +6,20 @@
       // If the icon has a video target selector in a `data-cu-video-target`
       // attribute, use that. Otherwise, use the closest video.
       let video_element = document.querySelector(event.target.dataset.cuVideoTarget) || event.target.closest('.cu-banner').querySelector('.cu-banner__media video');
-      let overlay_content = event.target.closest('.cu-banner__content');
+      let play = document.querySelector('.play');
+      let pause = document.querySelector('.pause');
 
-      if (video_element && video_element.paused) {
-        video_element.play();
-        overlay_content.style.display = 'none';
-        video_element.controls = true;
+      if (video_element) {
+        if (video_element.paused) {
+          video_element.play();
+          video_element.controls = true;
+          play.classList.add('visually-hidden');
+          pause.classList.remove('visually-hidden');
+        } else {
+          video_element.pause();
+          pause.classList.add('visually-hidden');
+          play.classList.remove('visually-hidden');
+        }
       }
     }
   });
