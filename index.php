98463df005685bd3d333a2352d3832aa8ec95f3a
Jeff Amaral <jeff.amaral@cornell.edu>
2022-07-01 12:20:42 -0400

    Update test script to output component info from inline docs

--- index.php
+++ index.php
@@ -2,18 +2,27 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-use Union\Union;
+use Union\Components;
 
-$union = new Union;
+$components = new Components;
 
-$components = $union->getComponents();
+foreach ($components->getComponents() as $component) {
+  echo '-------' . PHP_EOL;
+  echo $component->id() . PHP_EOL;
 
-foreach ($components as $component) {
-  // print_r($component);
-  // var_dump($component->template->getPath());
-  // var_dump($component->getLibrary());
+  if ($component->getDescription()) {
+    echo $component->getLabel() . PHP_EOL . PHP_EOL;
+    echo $component->getDescription() . PHP_EOL . PHP_EOL;
+    echo (new Parsedown())->text($component->getDescription()) . PHP_EOL . PHP_EOL;
+
+    foreach ($component->getTemplateVars() as $var) {
+      echo $var->getVariableName() . ' (' . $var->getType() . ')' . PHP_EOL;
+      echo '  ' . $var->getDescription() . PHP_EOL . PHP_EOL;
+    }
+  }
+  else {
+    echo PHP_EOL;
+  }
 }
 
-$banner = $union->getComponent('_logo');
 
-var_dump($banner);
