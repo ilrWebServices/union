fa2161d4e37ced2420bee4ebea90a5027f1fbe5d
Jeff Amaral <jeff.amaral@cornell.edu>
2023-11-15 11:18:49 -0500

    Switch the dismissible component to an HTML custom element
    Using the 'Light HTML element' technique described at https://meyerweb.com/eric/thoughts/2023/11/01/blinded-by-the-light-dom/

--- components/dismissible/dismissible.js
+++ components/dismissible/dismissible.js
@@ -1,33 +1,25 @@
-(function (window, document) {
+class cuDismissible extends HTMLElement {
+  connectedCallback() {
+    if (('localStorage' in window) === false) {
+      return;
+    }
 
-  if (('localStorage' in window) === false) {
-    return;
+    let digest = this.getAttribute('digest');
+    let checkbox = this.querySelector('.cu-dismissible__input');
+
+    // Has this element been dismissed? Tag it with a class if so.
+    if (parseInt(localStorage.getItem('cu-diss-' + digest))) {
+      checkbox.checked = true;
+    }
+    else {
+      // Store the dismissal state in local storage.
+      checkbox.addEventListener('change', function(event) {
+        localStorage.setItem('cu-diss-' + digest, event.target.checked ? 1 : 0);
+      }, false);
+    }
   }
+}
 
-  const processDismissibles = function() {
-    const dismissible_elements = document.querySelectorAll('[data-cu-digest].cu-dismissible');
-
-    for (const dismissible_element of dismissible_elements) {
-      // Has this element been dismissed? Tag it with a class if so.
-      if (parseInt(localStorage.getItem('cu-diss-' + dismissible_element.dataset.cuDigest))) {
-        dismissible_element.classList.add('cu-dismissible--predismissed')
-      }
-      else {
-        dismissible_element.classList.remove('cu-dismissible--predismissed');
-      }
-    }
-  };
-
-  let observer = new MutationObserver(processDismissibles).observe(document, {
-    childList: true,
-    subtree: true
-  });
-
-  // Store the dismissal state in local storage.
-  document.addEventListener('change', function(event) {
-    if (event.target.matches('.cu-dismissible__input')) {
-      localStorage.setItem('cu-diss-' + event.target.parentNode.dataset.cuDigest, event.target.checked ? 1 : 0);
-    }
-  }, false);
-
-})(window, document);
+if (('customElements' in window) === true) {
+  customElements.define('cu-dismissible', cuDismissible);
+}
