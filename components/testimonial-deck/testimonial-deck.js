a75ec084814edfe06ee93a7199d44b0d4b6e7e8a
Jeff Amaral <jeff.amaral@cornell.edu>
2025-11-07 14:47:13 -0500

    Refactor testimonial deck for mobile
    Desktop is next.
    
    Co-authored-by: Aaron Froehlich <aaronfroehlich@gmail.com>

--- components/testimonial-deck/testimonial-deck.js
+++ components/testimonial-deck/testimonial-deck.js
@@ -1,33 +1,154 @@
-(function (document) {
-  initDecks = function() {
-    const decks =  document.getElementsByClassName('cu-testimonial-deck');
-    for (let deck of decks) {
-      let testimonials = deck.getElementsByClassName('cu-testimonial');
-      let copy = testimonials[0].cloneNode(true);
-      copy.dataset.selected = 'selected';//('dataselected', 'selected');
-      deck.insertBefore(copy, testimonials[1]);
+(function(document) {
+
+  'use strict';
+
+  /**
+   * Define behavior for the <cu-testimonials> custom element.
+   */
+  class cuTestimonials extends HTMLElement {
+
+    #currentItem = 0;
+    #items;
+    #itemsArray;
+    #thumbs = [];
+    #elements = {
+      play_pause: document.createElement('div'),
+      thumb_container: document.createElement('ul')
+    };
+    #observer;
+    #timer;
+    #isVisible = false;
+
+    constructor() {
+      super();
+
+      // tabindex is set so that this component is focusable, but the value is
+      // -1 so it can't be reached by the `tab` key.
+      this.setAttribute('tabindex', '-1');
     }
-  };
 
-  document.addEventListener('click', function(event) {
-    if (event.target.matches('.cu-testimonial__media .cu-image')) {
-      let chosen = event.target.closest('.cu-testimonial');
+    connectedCallback() {
+      this.#items = this.querySelectorAll('.cu-testimonial');
+      this.#itemsArray = Array.from(this.#items);
 
-      if (chosen.getAttribute('data-current')) {
+      this.#elements.play_pause.classList.add('cu-testimonial-deck__playpause');
+      this.#elements.thumb_container.classList.add('cu-testimonial-deck__thumbnails');
+
+      this.append(this.#elements.play_pause);
+      this.append(this.#elements.thumb_container);
+
+      // TODO: Honor https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/At-rules/@media/prefers-reduced-motion
+      if (window.matchMedia(`(prefers-reduced-motion: reduce)`) === true || window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true) {
+        this.dataset.state = 'paused';
+      }
+
+      this.#observer = new IntersectionObserver((entries, observer) => {
+        for (const entry of entries) {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+            this.#thumbs.at(entry.target.dataset.item).classList.add('active');
+            this.#currentItem = this.#itemsArray.indexOf(entry.target);
+          }
+          else {
+            entry.target.classList.remove('active');
+            this.#thumbs.at(entry.target.dataset.item).classList.remove('active');
+          }
+        }
+      }, {
+        root: this,
+        rootMargin: "0px",
+        threshold: .5,
+      });
+
+      for (const item of this.#items) {
+        this.#observer.observe(item);
+        item.dataset.item = this.#itemsArray.indexOf(item);
+        let thumb = document.createElement('div');
+        thumb.classList.add('cu-testimonial-deck__thumbnail');
+        thumb.dataset.item = this.#itemsArray.indexOf(item);
+        this.#thumbs.push(thumb);
+        this.#elements.thumb_container.append(thumb);
+      }
+
+      // Set #isVisible if this deck is partially in the viewport.
+      const in_view_observer = new IntersectionObserver((entries, observer) => {
+        // Since only one item, this deck, is observed, the array will always only have one item.
+        this.#isVisible = entries[0].isIntersecting;
+      }, {
+        root: document,
+        threshold: .25,
+      });
+
+      in_view_observer.observe(this);
+
+      this.addEventListener("touchmove", (event) => {
+        if (event.target === this.#elements.play_pause) {
+          return;
+        }
+
+        if (this.dataset.state === 'playing') {
+          this.pause();
+        }
+      });
+
+      this.pause();
+
+      this.addEventListener('click', (event) => {
+        this.delegateClick(event);
+      });
+    }
+
+    delegateClick(event) {
+      if (event.target === this.#elements.play_pause) {
+        if (this.dataset.state === 'paused') {
+          this.play();
+        }
+        else {
+          this.pause();
+        }
+
         return;
       }
 
-      let copy = chosen.cloneNode(true);
-      let deck = event.target.closest('.cu-testimonial-deck');
-      let testimonials = deck.getElementsByClassName('cu-testimonial');
-      let previous = deck.querySelectorAll('[data-selected="selected"]');
-      testimonials[0].remove();
-      deck.insertBefore(copy, testimonials[0]);
-      copy.dataset.current = 'current';
-      delete previous[0].dataset.selected;
-      chosen.dataset.selected = 'selected';
-    }
-  }, false);
+      if (this.#thumbs.includes(event.target)) {
+        this.#currentItem = event.target.dataset.item;
+        this.moveCenter(this.#items[this.#currentItem]);
+      }
 
-  initDecks();
+      this.pause();
+    }
+
+    next() {
+      this.#currentItem++;
+      if (this.#currentItem > this.#items.length - 1) {
+        this.#currentItem = 0;
+      }
+      this.moveCenter(this.#items[this.#currentItem]);
+    }
+
+    prev() {}
+
+    pause() {
+      this.dataset.state = 'paused';
+      clearInterval(this.#timer);
+    }
+
+    play() {
+      this.dataset.state = 'playing';
+      this.#timer = setInterval(() => {
+        if (this.#isVisible) {
+          this.next();
+        }
+      }, 5000);
+      this.next();
+    }
+
+    moveCenter(item) {
+      item.scrollIntoView({ behavior: "smooth", block: "start", inline: "center" });
+    }
+
+  }
+
+  customElements.define("cu-testimonial-deck", cuTestimonials);
+
 })(document);
