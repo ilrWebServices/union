b326602b3c7273e7377ce6bbb846391041d86b4d
gregwarren <greg.warren@primacy.com>
2025-11-20 17:54:48 -0500

    Add call-to-action component with banner and mission statement styles

---
+++ components/call-to-action/call-to-action.js
@@ -0,0 +1,15 @@
+/**
+ * Call to Action component
+ */
+
+(function (Drupal) {
+  'use strict';
+
+  Drupal.behaviors.callToAction = {
+    attach: function (context, settings) {
+      // Add any JavaScript functionality for the call to action component here
+      // Currently, this component doesn't require any JavaScript
+    }
+  };
+
+})(Drupal);
